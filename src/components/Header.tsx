38699900bc1107e4570314cf6853434907e49fcf
junayedhasan302 <junayedhasan302@gmail.com>
2026-09-14 01:05:54 +0600

    Centered DIV

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
-
 const Header = () => {
-    return (
-        <div className='container bg-green-100 h-[700px] w-[500px] border rounded-2xl text-center '>
-            <h1 className='text-red-600 font-bold text-2xl'>My Task Manager</h1>
-            <h2 className='text-green-700 text-lg'>Manage your daily tasks</h2>
-            <hr className="" />
-        </div>
-    );
+  return (
+    <div className="absolute inset-0 m-auto h-[700px] w-[500px] rounded-2xl border bg-green-100 text-center">
+      <h1 className="text-red-600 font-bold text-2xl">My Task Manager</h1>
+      <h2 className="text-green-700 text-lg">Manage your daily tasks</h2>
+      <hr className="" />
+    </div>
+  );
 };
 
 export default Header;
