ddd92e7731d2623a367cfd85012b827d23bcceca
junayedhasan302 <junayedhasan302@gmail.com>
2026-09-14 01:02:38 +0600

    Todo list initial codes here

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div className='container bg-green-100 h-[700px] w-[500px] border rounded-2xl text-center '>
+            <h1 className='text-red-600 font-bold text-2xl'>My Task Manager</h1>
+            <h2 className='text-green-700 text-lg'>Manage your daily tasks</h2>
+            <hr className="" />
+        </div>
+    );
+};
+
+export default Header;
